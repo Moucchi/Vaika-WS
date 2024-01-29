64aac22642c53241003dfdb704f6432a63673dbb
fitahianatony <fitahianatony77@gmail.com>
2024-01-29 05:55:17 +0300

    new file:   src/main/java/org/ITU/S5/cloud/backOffice/controller/ImageUploadController.java 	new file:   src/main/java/org/ITU/S5/cloud/storage/FileSystemStorageService.java 	new file:   src/main/java/org/ITU/S5/cloud/storage/StorageException.java 	new file:   src/main/java/org/ITU/S5/cloud/storage/StorageFileNotFoundException.java 	new file:   src/main/java/org/ITU/S5/cloud/storage/StorageProperties.java 	new file:   src/main/java/org/ITU/S5/cloud/storage/StorageService.java 	modified:   src/main/resources/application.properties 	modified:   target/classes/application.properties 	new file:   target/classes/org/ITU/S5/cloud/storage/FileSystemStorageService.class 	new file:   target/classes/org/ITU/S5/cloud/storage/StorageException.class 	new file:   target/classes/org/ITU/S5/cloud/storage/StorageFileNotFoundException.class 	new file:   target/classes/org/ITU/S5/cloud/storage/StorageProperties.class 	new file:   target/classes/org/ITU/S5/cloud/storage/StorageService.class

---
+++ src/main/java/org/ITU/S5/cloud/backOffice/controller/ImageUploadController.java
@@ -0,0 +1,38 @@
+package org.ITU.S5.cloud.backOffice.controller;
+
+import org.ITU.S5.cloud.storage.StorageFileNotFoundException;
+import org.ITU.S5.cloud.storage.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class ImageUploadController {
+    private final StorageService storageService;
+
+    @Autowired
+    public ImageUploadController(StorageService storageService) {
+		this.storageService = storageService;
+	}
+
+    @PostMapping("/")
+	public String handleFileUpload(@RequestParam("file") MultipartFile file,
+			RedirectAttributes redirectAttributes) {
+
+		storageService.store(file);
+		redirectAttributes.addFlashAttribute("message",
+				"You successfully uploaded " + file.getOriginalFilename() + "!");
+
+		return "redirect:/";
+	}
+
+	@ExceptionHandler(StorageFileNotFoundException.class)
+	public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
+		return ResponseEntity.notFound().build();
+	}
+}
---
+++ src/main/java/org/ITU/S5/cloud/storage/StorageFileNotFoundException.java
@@ -0,0 +1,12 @@
+package org.ITU.S5.cloud.storage;
+
+public class StorageFileNotFoundException extends StorageException {
+
+	public StorageFileNotFoundException(String message) {
+		super(message);
+	}
+
+	public StorageFileNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
