f3ac607c918ddac9f514fecb3fe608238a95b091
Emmanuel Rafidinjanahary <120130815+Moucchi@users.noreply.github.com>
2024-02-09 23:40:34 +0300

    Ajout index pour test

--- src/main/java/org/ITU/S5/cloud/backOffice/controller/Test.java
+++ src/main/java/org/ITU/S5/cloud/backOffice/controller/Test.java
@@ -1,14 +1,14 @@
 package org.ITU.S5.cloud.backOffice.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/v1/tests")
 public class Test {
     @GetMapping
     public String test() {
-        return "Aonaaaaaa";
+        return "index";
     }
 }
